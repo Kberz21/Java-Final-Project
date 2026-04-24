3c71b23b0a6ef85f0a57af335b1741b537822592
Kberz21 <2025446serey@aupp.edu.kh>
2026-04-24 21:59:43 +0700

    new file structure

---
+++ src/com/financialtracking/Transaction.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private  double amount;
+    private String remark;
+    private boolean isIncome;
+    private LocalDate eventDate;
+    private LocalDateTime createdTimestamp;
+
+    public Transaction(double Amount, String Remark){
+        this.amount= Amount;
+        this.remark= Remark;
+        this.isIncome= true;
+        this.eventDate= LocalDate.now();
+        this.createdTimestamp= LocalDateTime.now();
+    }
+
+    public void isExpense(){
+        this.isIncome=false;
+    }
+    
+}
